cd1343a70a7541970dd580e965e295f5179b8529
zidanredha2 <amazinginflation58@gmail.com>
2025-10-22 16:03:33 +0530

    lambda expressions practice

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ class Main {
     public static void main(String[] args) {
        Hero hero = new Hero();
        Villain villain = new Villain();
+       List<Integer> list = Arrays.asList(1,2,3,4,5);
+       list.forEach(i -> doubleit(i));
        hero.act();
        villain.act();
        hero.stop();
@@ -13,5 +15,8 @@ class Main {
        Hello hello = name -> System.out.println("Hello, " + name + "!");
        hello.sayHello("Zidan");
        hello.greet("Zidan");
+    }
+    static void doubleit(int i){
+        System.out.println(i*2);
     }
 }
